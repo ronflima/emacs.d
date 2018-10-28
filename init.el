6bd40c199e343a093c2c4086ec54fdc178c85bed
Ronaldo Faria Lima <ronaldo@chicletemkt.com>
2018-10-28 16:15:03 -0300

    Added auto-complete configuration

--- init.el
+++ init.el
@@ -175,3 +175,10 @@
 ;; ido mode
 (require 'ido)
 (ido-mode t)
+
+;; auto complete
+(require 'auto-complete)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
+(require 'auto-complete-config)
+(ac-config-default)
+(global-auto-complete-mode t)
